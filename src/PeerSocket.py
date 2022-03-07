474e8dddd153847bfe665b25284edda66e10f517
Alex Bethel <abethel@cs.nmt.edu>
2022-03-06 18:19:07 -0700

    Implement key exchange

--- src/PeerSocket.py
+++ src/PeerSocket.py
@@ -3,8 +3,19 @@
    concerned with is connecting to single other computers via TCP/IP,
    exchanging public keys, and getting secure connections."""
 
-from typing import Callable, cast
+from typing import Callable
 import socket
+
+from cryptography.hazmat.primitives.asymmetric.x25519 import (
+    X25519PrivateKey as PrivateKey,
+    X25519PublicKey as PublicKey,
+)
+from cryptography.hazmat.primitives.serialization import (
+    Encoding,
+    PublicFormat,
+)
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 # Maximum number of simultaneous connections to allow in the backlog.
 # This affects basically nothing.
@@ -23,7 +34,7 @@ class EncryptedStream:
     def __init__(
             self,
             sock: socket.socket,
-            key: bytearray,
+            key: bytes,
             buf: bytearray,
     ) -> None:
         """Creates a new EncryptedStream that promises that the given socket
@@ -37,22 +48,23 @@ class EncryptedStream:
     def connect(
             other_addr: str,
             other_port: int,
-            our_private_key: str,
-            their_public_key: Callable[[str], bool],
+            private_key: PrivateKey,
+            key_checker: Callable[[PublicKey], bool],
     ) -> EncryptedStream:  # noqa: F821
         """Establishes a connection to `other_addr`. Authenticates the
-           connection using `our_private_key`. Passes the public key
-           the peer sends through the provided `their_public_key`
-           function; if the function returns `true`, then we accept
-           their public key, and if it returns `false`, we abort the
-           connection and raise an exception."""
+           connection using `private_key`. Passes the public key the
+           peer sends through the provided `key_checker` function; if
+           the function returns `true`, then we accept their public
+           key, and if it returns `false`, we abort the connection and
+           raise an exception."""
         sock = socket.socket()
         sock.connect((other_addr, other_port))
-
         buf = bytearray()
+
         magic_number_check(sock, buf)
 
-        key = cast(bytearray, print('TODO: implement key exchange'))
+        key = key_exchange(sock, buf, private_key, key_checker)
+
         return EncryptedStream(sock, key, buf)
 
     def send(self, data: bytearray) -> None:
@@ -85,16 +97,16 @@ class EncryptedListener:
             self,
             addr: str,
             port: int,
-            our_private_key: str,
-            their_public_key: Callable[[str], bool],
+            our_private_key: PrivateKey,
+            key_checker: Callable[[PublicKey], bool],
     ) -> None:
         """Create a new listener socket with the given address, port, and
            private key. When remote peers connect to this listener,
-           pass the public key they send through the
-           `their_public_key` function; if and only if it returns
-           `true`, use their public key and our private key to
-           negotiate a secure, encrypted connection, which is yielded
-           as an `EncryptedStream`.
+           pass the public key they send through the `key_checker`
+           function; if and only if it returns `true`, use their
+           public key and our private key to negotiate a secure,
+           encrypted connection, which is yielded as an
+           `EncryptedStream`.
 
            Throws an exception if binding to the address-port pair
            fails."""
@@ -104,16 +116,32 @@ class EncryptedListener:
         self._sock.bind((addr, port))
         self._sock.listen(BACKLOG)
 
+        self._private_key = our_private_key
+        self._key_checker = key_checker
+
     def accept(self) -> EncryptedStream:
         """Waits for the next valid, encrypted connection to the listener
            socket, and returns it as an encrypted stream."""
-        sock, _ret_addr = self._sock.accept()
+        while True:
+            try:
+                sock, _ret_addr = self._sock.accept()
+                buf = bytearray()
 
-        buf = bytearray()
-        magic_number_check(sock, buf)
+                # BUG: These are blocking operations, which will lock
+                # up the main thread if someone connects and then
+                # doesn't send any data.
+                magic_number_check(sock, buf)
 
-        key = cast(bytearray, print('TODO: implement key exchange'))
-        return EncryptedStream(sock, key, buf)
+                key = key_exchange(
+                    sock,
+                    buf,
+                    self._private_key,
+                    self._key_checker
+                )
+                return EncryptedStream(sock, key, buf)
+
+            except ProtocolException as e:
+                print('Warning: rejected incoming connection: ' + str(e))
 
 
 def magic_number_check(sock: socket.socket, buf: bytearray) -> None:
@@ -121,15 +149,67 @@ def magic_number_check(sock: socket.socket, buf: bytearray) -> None:
        machine on the other end to return the same magic number. Uses
        `buf` as storage for buffering on the socket."""
     sock.send(MAGIC)
+    if read_exact(sock, buf, len(MAGIC)) != MAGIC:
+        raise ProtocolException('received incorrect magic number')
 
-    while len(buf) < len(MAGIC):
+
+def key_exchange(
+        sock: socket.socket,
+        buf: bytearray,
+        private_key: PrivateKey,
+        key_checker: Callable[[PublicKey], bool],
+) -> bytes:
+    """Performs a key exchange with over the socket with the given private
+       key. Checks the public key the peer sends with the functin
+       `key_checker`; raises an exception if that function returns
+       `False`."""
+    our_pk_bytes = private_key.public_key().public_bytes(
+        encoding=Encoding.Raw,
+        format=PublicFormat.Raw,
+    )
+
+    # Send and receive public keys prefixed by their lengths as 32-bit
+    # (4-byte) integers.
+    sock.send(bytearray(len(our_pk_bytes).to_bytes(4, 'big')) + our_pk_bytes)
+    their_pk_len = int.from_bytes(read_exact(sock, buf, 4), 'big')
+    their_pk_bytes = read_exact(sock, buf, their_pk_len)
+    their_pk = PublicKey.from_public_bytes(their_pk_bytes)
+
+    if not key_checker(their_pk):
+        raise ProtocolException("rejected peer's public key")
+
+    shared_key = private_key.exchange(their_pk)
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        # I'm 85% certain these should both be None; need to do
+        # further research to be positive, though. ~~Alex
+        salt=None,
+        info=None,
+    ).derive(shared_key)
+
+    return derived_key
+
+
+def read_exact(
+        sock: socket.socket,
+        buf: bytearray,
+        length: int,
+) -> bytearray:
+    """Reads exactly `length` bytes from `sock`, using `buf` to store
+       extra bytes read. Throws an exception if less than `length`
+       bytes can be read from the socket."""
+    while len(buf) < length:
         nextbuf = sock.recv(BUFSIZE)
+        if len(nextbuf) == 0:
+            raise ProtocolException('unexpected EOF')
+
         buf += nextbuf
 
-    if not buf.startswith(MAGIC):
-        raise ProtocolException("Received incorrect magic number")
+    read = buf[0:length]
+    del buf[0:length]
 
-    del buf[0:len(MAGIC)]
+    return read
 
 
 class ProtocolException(Exception):
