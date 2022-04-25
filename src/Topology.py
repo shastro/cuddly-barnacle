caf3a44c389e592c93f4502ec6ac121fc9635a5f
Alex Bethel <abethel@cs.nmt.edu>
2022-04-24 21:16:36 -0600

    Node state work

--- src/Topology.py
+++ src/Topology.py
@@ -7,12 +7,18 @@ state of the network.
 """
 
 from abc import ABC
+from typing import List
+import random
+
+from EncryptedStream import EncryptedStream, PublicKey, PrivateKey
+
+PORT_NUMBER = 18457
 
 
 class NodeState(ABC):
     """The network state of a single node."""
 
-    def __init__(self) -> None:
+    def run(self) -> 'NodeState':
         pass
 
 
@@ -25,7 +31,15 @@ class StableState(NodeState):
 
     """
 
-    def __init__(self):
+    def __init__(
+            self,
+            predecessor: EncryptedStream,
+            successor: EncryptedStream,
+    ):
+        self._pred = predecessor
+        self._succ = successor
+
+    def run(self) -> NodeState:
         pass
 
 
@@ -38,7 +52,17 @@ class HubState(NodeState):
 
     """
 
-    def __init__(self):
+    def __init__(
+            self,
+            predecessor: EncryptedStream,
+            successor: EncryptedStream,
+            extra: EncryptedStream,
+    ):
+        self._pred = predecessor
+        self._succ = successor
+        self._extra = extra
+
+    def run(self) -> NodeState:
         pass
 
 
@@ -50,7 +74,13 @@ class SpokeState(NodeState):
 
     """
 
-    def __init__(self):
+    def __init__(
+            self,
+            entry_point: EncryptedStream,
+    ):
+        self._entry_point = entry_point
+
+    def run(self) -> NodeState:
         pass
 
 
@@ -62,8 +92,42 @@ class SolitaryState(NodeState):
 
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         pass
+
+    def run(self) -> NodeState:
+        pass
+
+
+def initialize(
+        peer_addrs: List[str],
+        peer_keys: List[PublicKey],
+        private_key: PrivateKey,
+) -> NodeState:
+    """Connects to the chat network, and returns a new NodeState.
+
+    Attempts to connect to a list of peer addresses in a random order,
+    and only connects to peers whose public keys are approved
+    according to the `peer_keys` list. Uses `private_key` to
+    authenticate to the peers.
+
+    """
+    peer_addrs_shuf = peer_addrs.copy()
+    random.shuffle(peer_addrs_shuf)
+    for addr in peer_addrs_shuf:
+        try:
+            connection = EncryptedStream.connect(
+                addr,
+                PORT_NUMBER,
+                private_key,
+                lambda key: key in peer_keys
+            )
+            return SpokeState(entry_point=connection)
+        except ConnectionRefusedError or TimeoutError as _err:
+            # Peer is offline, just try other peers.
+            id(_err)
+            pass
+    return SolitaryState()
 
 
 def topology_test() -> None:
