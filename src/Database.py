d1fb303cbeec66f89661bbd4d9c0f0fde8fbd300
shastro <guitarshughes@hotmail.com>
2022-04-24 23:02:20 -0600

    Database updates. Add environment class for basic global state of application

--- src/Database.py
+++ src/Database.py
@@ -1,5 +1,12 @@
-"""Generic Database Interface"""
+"""SQLite Database Interface abstraction over sqlite3
 
+This file implements the database interface for the application data on the system. Typically stored under ~/.chatchat/database [Defined in Environment.py]
+
+This system is interfaced with the synchronization system as well as the frontend.
+
+"""
+import hashlib
+from _hashlib import HASH
 import datetime
 import logging
 import sqlite3
@@ -7,17 +14,143 @@ from abc import ABC, abstractclassmethod, abstractmethod, abstractproperty
 from enum import Enum, auto, unique
 from ipaddress import IPv4Address
 from sqlite3.dbapi2 import Date
-from typing import Any, Iterable, Optional, Union
+from typing import Any, Iterable, Optional, Union, List
 
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric.x25519 import (
-    X25519PrivateKey as PrivateKey,
-    X25519PublicKey as PublicKey,
-)
+# Project Imports
+from EncryptedStream import PublicKey, PrivateKey
+
+
+# @unique
+# class QueryType(Enum):
+#     """Enum to identify the different query types"""
+
+#     HASH = auto()
+#     EVENT = auto()
+#     IP = auto()
+#     PUBKEY = auto()
+
+
+class TimeQuery:
+    def __init__(self, start: datetime.datetime, end: datetime.datetime):
+        self.time_start = start
+        self.time_end = end
+
+
+class HashQuery:
+    def __init__(self, hashes: List[HASH]):
+        self._hashes = hashes
+
+
+class SQLiteDB:
+    """SQLiteDB abstraction over the sqlite3 interface.
+
+    Designed to be used with our system and supportsthe specific kinds of queries we are interested in.
+
+    """
+
+    def __init__(self) -> None:
+        """Instantiate and set filename"""
+        self.fname = None
+        self.connection = None
+        self.cursor = None
+        self.changed = False
+        self.committed = False
+
+    def createEmpty(self, ifname: str, force: bool = False):
+        """Create an empty database with our specified format.
+
+        Will not by default overwrite an existing database unless the `force` flag is specified.
+        This function establishes connection to the sqlite3 file and creates a cursor object.
+
+        """
+
+        if self.connection is not None:
+            raise ConnectionAlreadyExists(self.fname)
+
+        self.fname = ifname
+        self.connection = sqlite3.connect(self.fname)
+        self.cursor = self.connection.cursor()
+
+    def connect(self):
+        """Connect to the database for operations. Will also create a new database"""
+        if self.connection is not None:
+            raise ConnectionAlreadyExists(self.fname)
+
+    def commit(self):
+        """Commit changes to the database."""
+
+        self.committed = True
+
+    def close(self):
+        """Close the database connection."""
+
+        if self.connection is None:
+            raise ConnectionAlreadyClosed(self.fname)
+
+        if self.changed and self.committed is False:
+            logging.warning("Closed connection without committing changes")
+        self.connection.close()
+
+    def eventQuery(self, query: Union[TimeQuery, HashQuery]):
+        """Return the requested events based on either a TimeQuery, or HashQuery
+
+        Also returns the timing information, the associated hash, and the event blob.
+        """
+
+        if type(query) == TimeQuery:
+            pass
+
+        elif type(query) == HashQuery:
+            pass
+
+        else:
+            raise InvalidQuery("Invalid Query Type in Function eventQuery")
+
+    def ipQuery(self):
+        """Return all the valid ip addresses in the database"""
+        pass
+
+    def pubkeyQuery(self):
+        """Return the valid publickeys in the database"""
+        pass
+
+    def hashQuery(self):
+        """Return the valid list of hashes in the database"""
+        pass
+
+    def updatePubKeys(self, keys: Iterable[PublicKey], sync: bool = False):
+        """Will update the list of publicKeys in the database.
+
+        If the sync flag is set the function will erase all the publickeys in the database and replace with the new set.
+
+        """
+        pass
+
+    def delete_pub_keys(self, keys: Iterable[PublicKey]):
+        """Delete all public keys that match those in the list `keys`"""
+
+        pass
+
+    def updateIPs(self, ips: Iterable[IPv4Address], overwrite: bool = False):
+        """Update all the ip addresses"""
+        pass
+
+    def updateEvents(
+        self, pairs: Iterable[tuple[bytes, bytes]], overwrite: bool = False
+    ):
+        pass
 
 
 class DatabaseException(Exception):
     """Base class for database exceptions"""
+
+
+class InvalidQuery(DatabaseException):
+    """Raised when there is an invalid query"""
+
+    def __init__(self, message: Optional[str]):
+        self.message = message
+        super().__init__(self.message)
 
 
 class ConnectionAlreadyExists(DatabaseException):
@@ -37,154 +170,6 @@ class ConnectionAlreadyClosed(DatabaseException):
         self.connection_name = iconnection_name
         self.message = f"Connection <{self.connection_name}> does not exist and could not be closed."
         super().__init__(self.message)
-
-
-"""Still Not sure how I am going to do Queries"""
-
-
-@unique
-class Query(Enum):
-    """Enum to identify the different query types"""
-
-    HASH = auto()
-    EVENT = auto()
-    IP = auto()
-    PUBKEY = auto()
-
-
-class TimeQuery:
-    def __init__(
-        self, start: datetime.datetime, end: datetime.datetime, query_type: QueryType
-    ):
-        self.time_start = start
-        self.time_end = end
-        self.item = query_type
-
-
-class MatchQuery(ABC):
-    """An abstract base class representing a database query that matches some list of properties exactly
-    IE these types of queries
-    Works to select a property from a column of a table provided it is on the same row as at least one property queried.
-    """
-
-    def __init__():
-        pass
-
-
-class HashQuery(MatchQuery):
-    pass
-
-
-class Database(ABC):
-    """Interface for an arbitrary database. Assumes a relational database of some form."""
-
-    @abstractmethod
-    def createEmpty(self, name: str):
-        """CreateEmpty should initialize a database with the name given as a string.
-        For local databases this should be a filename, for remote databases this could be used
-        as some form of id or not at all"""
-        pass
-
-    @abstractmethod
-    def connect(self):
-        pass
-
-    @abstractmethod
-    def close(self):
-        pass
-
-    @abstractmethod
-    def eventQuery(self):
-        pass
-
-    @abstractmethod
-    def ipQuery(self):
-        pass
-
-    @abstractmethod
-    def pubkeyQuery(self):
-        pass
-
-    @abstractmethod
-    def hashQuery(self, HashQuery) -> HashQuery:
-        pass
-
-    @abstractmethod
-    def updatePubKeys(self, keys: Iterable[PublicKey], overwrite: bool = False):
-        pass
-
-    @abstractmethod
-    def updateIPs(self, ips: Iterable[IPv4Address], overwrite: bool = False):
-        pass
-
-    @abstractmethod
-    def updateEvents(
-        self, pairs: Iterable[tuple[bytes, bytes]], overwrite: bool = False
-    ):
-        pass
-
-
-class SQLiteDB(Database):
-    def __init__(self) -> None:
-        """Instantiate and set filename"""
-        self.fname = None
-        self.connection = None
-        self.cursor = None
-        self.changed = False
-        self.committed = False
-
-    def createEmpty(self, ifname: str, force: bool = False):
-        """Create an empty database with our specified format.
-        Will not by default overwrite an existing database."""
-
-        if self.connection is not None:
-            raise ConnectionAlreadyExists(self.fname)
-        self.fname = ifname
-        self.connection = sqlite3.connect(self.fname)
-        self.cursor = self.connection.cursor()
-
-    def connect(self):
-        """Connect to the database for operations. Will also create a new database"""
-        if self.connection is not None:
-            raise ConnectionAlreadyExists(self.fname)
-
-    def commit(self):
-        """Commit changes to the database."""
-
-        self.committed = True
-
-    def close(self):
-        """Close the database connection."""
-        if self.connection is None:
-            raise ConnectionAlreadyClosed(self.fname)
-
-        if self.changed and self.committed is False:
-            logging.warning("Closed connection without committing changes")
-        self.connection.close()
-
-    def eventQuery(self, query: Union[TimeQuery, HashQuery]):
-        """Find all the requested events based on either a TimeQuery, or HashQuery"""
-        pass
-
-    def ipQuery(self):
-        pass
-
-    def pubkeyQuery(self):
-        pass
-
-    def hashQuery(self):
-        pass
-
-    def updatePubKeys(self, keys: Iterable[PublicKey], overwrite: bool = False):
-        pass
-
-    def updateIPs(self, ips: Iterable[IPv4Address], overwrite: bool = False):
-        pass
-
-    def updateEvents(
-        self, pairs: Iterable[tuple[bytes, bytes]], overwrite: bool = False
-    ):
-        pass
 
 
 def main():
--- src/Environment.py
+++ src/Environment.py
@@ -1,10 +1,36 @@
 # Class for storing global application environment database
 
 import os
+from builtins import hasattr
+from pathlib import PurePath, Path
 
 
 class Env:
-    def __init__(self, subdir: os.PathLike):
+    """Singleton Class to capture global program state related to the environment the application runs in"""
+
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(Env, cls).__new__(cls)
+            return cls.instance
+
+    def __init__(self):
         homedir = os.getenv("HOME")
         if homedir:
             self.root = homedir
+            self.database = PurePath(homedir, "database", "client.db")
+
+    def get_root_path(self):
+        return self.root
+
+    def get_database_path(self):
+        return self.database
+
+
+def tests():
+    env = Env()
+    print(env.get_root_path())
+    print(env.get_database_path())
+
+
+if __name__ == "__main__":
+    tests()
