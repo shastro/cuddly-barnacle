c096dd25e00072acb896c18601b7744a0bd0659f
Alex Bethel <abethel@cs.nmt.edu>
2022-05-01 14:08:25 -0600

    Fix `api` object name

--- src/API.py
+++ src/API.py
@@ -3,7 +3,8 @@
 from flask import Flask, request, render_template, url_for
 import json
 
-api = Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,41 +12,42 @@ def index():
     # text = request.form['From']
     return render_template('index.html')
 
-@api.route('/messages', methods=['GET'])
+
+@app.route('/messages', methods=['GET'])
 def messages():
     """Retrieves a list of messages falling within a time range."""
     pass
 
 
-@api.route('/post', methods=['POST'])
+@app.route('/post', methods=['POST'])
 def post():
     """Posts a message to the channel."""
     pass
 
 
-@api.route('/invite', methods=['POST'])
+@app.route('/invite', methods=['POST'])
 def invite():
     """Invites a new user to the channel."""
     pass
 
 
-@api.route('/ban', methods=['POST'])
+@app.route('/ban', methods=['POST'])
 def ban():
     """Removes a user from the channel."""
     pass
 
 
-@api.route('/enter', methods=['POST'])
+@app.route('/enter', methods=['POST'])
 def enter():
     """Indicates to other users that you are online."""
     pass
 
 
-@api.route('/leave', methods=['POST'])
+@app.route('/leave', methods=['POST'])
 def leave():
     """Indicates to other users that you are offline."""
     pass
 
 
 if __name__ == '__main__':
-    api.run()
+    app.run()
